2ac2c275beae927f344de0cd539c5f49dccb0cba
MolchanovSergii <molchanse@gmail.com>
2023-04-14 15:08:47 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,13 +2,12 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const startBtn = document.querySelector('[data-start]');
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
+const day = document.querySelector('[data-days]');
+const hour = document.querySelector('[data-hours]');
+const minute = document.querySelector('[data-minutes]');
+const second = document.querySelector('[data-seconds]');
 
 startBtn.disabled = true;
-let userDate = null;
 
 const options = {
     enableTime: true,
@@ -16,17 +15,51 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log('selectedDates[0]', selectedDates[0]);
-        userDate = selectedDates[0];
-        console.log('userDate внутри настроеек: ', userDate)
+        const startCountDate = Date.now();
+        const endCountDate = selectedDates[0].getTime();
+        const deltaCount = endCountDate - startCountDate;
+
+        if (deltaCount <= 0) {
+            window.alert("Please choose a date in the future");
+        }
+
+        startBtn.disabled = false;
+
+        startBtn.addEventListener('click', onStart);
+        
+        function onStart() {
+
+            const intervalId = setInterval(() => {
+                
+                const { days, hours, minutes, seconds } = convertMs(deltaCount);
+                day.textContent = days;
+                hour.textContent = hours;
+                minute.textContent = minutes;
+                second.textContent = seconds;
+                deltaCount -= 1000;
+
+            }, 1000)
+
+        }   
+
     },
 };
 
-
 flatpickr('#datetime-picker', options);
 
-console.log('userDate снаружи настроеек: ', userDate);
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
 
 
 
