95f3ccc2387a093974bf5a716f155fab80f62c6a
MolchanovSergii <molchanse@gmail.com>
2023-04-15 18:04:31 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,7 @@
+const form = document.querySelector('.form');
+
+console.dir(form.elements)
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
