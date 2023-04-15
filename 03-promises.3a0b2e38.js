1a966d57a5ac8783f1d042737fb29b9c82b27427
MolchanovSergii <118202123+MolchanovSergii@users.noreply.github.com>
2023-04-15 15:04:55 +0000

    Deploying to gh-pages from @ MolchanovSergii/goit-js-hw-09@95f3ccc2387a093974bf5a716f155fab80f62c6a 🚀

---
+++ 03-promises.3a0b2e38.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form");console.dir(e.elements)}();
+//# sourceMappingURL=03-promises.3a0b2e38.js.map
---
+++ 03-promises.433c76e9.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");console.dir(e.elements);
+//# sourceMappingURL=03-promises.433c76e9.js.map
